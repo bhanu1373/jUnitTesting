33c894eb541128ecf73f7f21399b4b7ebc88d377
bhanu1373 <bhanuprakashreddygundam@gmail.com>
2022-04-23 21:46:46 +0530

    testing

---
+++ src/jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,10 @@
+package jUnitTestingPackage;
+ 
+public class jUnitFunction {
+	public int addnumber(int num1, int num2) {
+		return num1+num2;
+	}
+	public String addstrings(String s1, String s2) {
+		return s1+s2;
+	}
+}
---
+++ src/jUnitTestingPackage/testAddNumbers.java
@@ -0,0 +1,18 @@
+package jUnitTestingPackage;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class testAddNumbers{
+
+
+	@Test
+	public void test() {
+		jUnitFunction junit = new jUnitFunction();
+		int result = junit.addnumber(100, 200);
+		assertEquals (300,result);
+
+	}
+
+}
